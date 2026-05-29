1523ab0272047c158777ebbc26de115e108ebc8d
JATTsamson <jattsamson@gmail.com>
2026-05-29 12:29:18 +0530

    Add files via upload
    it is the updated one,

--- matchcase&conditions.py
+++ matchcase&conditions.py
@@ -1,4 +1,4 @@
-def main():
+def main1():
     designation=input("designation:").lower()
     match designation:
         case "admin"|"super_user":
@@ -10,4 +10,27 @@ def main():
         case _:
             print("who?, ohh you are for internship? so cute")
 
+main1()
+
+"""
+it was easy ngl, 
+ig the idea is still the same, make the code easy to read over smaller,
+cuz you dont know what is happening the machine
+
+learned match/case, if elif else, and conditions today fr
+"""
+
+def main ():
+    user_input=int(input("x:"))
+    if height(user_input):
+        print("positive")
+    else:
+        print("negetive")
+#if true it is positive
+#if false it is negetive
+
+
+def height(value):
+    return value>0 
+#think it will always send true or false
 main()
